56db85709cc9431d81abd784ae72458555e83379
Grace_ngigi <gracengigi859@gmail.com>
2024-01-15 15:23:36 -0800

    Auth Class

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -3,18 +3,18 @@
 from flask import request
 from typing import List, TypeVar
 
+
 class Auth:
     ''' class to manage the API authentication '''
-    pass
 
-def require_auth(self, path: str, excluded_paths: List[str]):
-    '''  returns False - path and excluded_paths '''
-    return False
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        '''  returns False - path and excluded_paths '''
+        return False
 
-def authorization_header(self, request=None) -> str:
-    ''' returns None '''
-    return None
+    def authorization_header(self, request=None) -> str:
+        ''' returns None '''
+        return None
 
-def current_user(self, request=None) -> TypeVar('User'):
-    ''' returns None '''
-    return None
+    def current_user(self, request=None) -> TypeVar('User'):
+        ''' returns None '''
+        return None
