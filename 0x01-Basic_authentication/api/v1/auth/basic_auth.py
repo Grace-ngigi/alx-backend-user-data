e18249cc057bf03e00a4e287d335849861fadaf6
Grace_ngigi <gracengigi859@gmail.com>
2024-01-15 16:13:58 -0800

    Basic auth

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+''' class BasicAuth that inherits from Auth '''
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    '''BasicAuth inerits from Auth'''
+    pass
