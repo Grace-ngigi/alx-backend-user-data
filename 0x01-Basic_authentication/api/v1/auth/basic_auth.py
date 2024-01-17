d4658e25674bbd43185e7c8d5b6442a2d42c407f
Grace-ngigi <gracengigi859@gmail.com>
2024-01-17 05:27:17 +0300

    Basic - Allow password with

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -46,7 +46,7 @@ class BasicAuth(Auth):
         if ':' not in decoded_base64_authorization_header:
             return (None, None)
         email = decoded_base64_authorization_header.split(":")[0]
-        passwd = decoded_base64_authorization_header.split(":")[1]
+        passwd = decoded_base64_authorization_header[len(email) + 1:]
         return (email, passwd)
 
     def user_object_from_credentials(self, user_email:
