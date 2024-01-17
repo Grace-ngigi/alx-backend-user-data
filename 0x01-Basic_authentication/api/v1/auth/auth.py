20328764819ebfd482412f7621a45a7c3b58a313
Grace_ngigi <gracengigi859@gmail.com>
2024-01-16 18:37:34 -0800

    Require auth with stars

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -16,7 +16,8 @@ class Auth:
 
         path = path.rstrip('/') + '/'
         for excluded_path in excluded_paths:
-            if excluded_path.endswith('*') and path.startswith(excluded_path[:-1]):
+            if excluded_path.endswith('*') and path.startswith(
+               excluded_path[:-1]):
                 return False
         return True
 
