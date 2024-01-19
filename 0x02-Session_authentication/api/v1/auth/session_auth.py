90f59a0310689cb3eea1e42e4b91bdaf701fc2cc
Grace-ngigi <gracengigi859@gmail.com>
2024-01-19 05:59:15 +0300

    Before request

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -4,6 +4,7 @@ Session Auth class definition
 """
 from .auth import Auth
 from uuid import uuid4
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -23,3 +24,10 @@ class SessionAuth(Auth):
         if not isinstance(session_id, str) or session_id is None:
             return None
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """Returns a User instance based on a cookie value"""
+        session_cookie = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(session_cookie)
+        return User.get(user_id)
+    
