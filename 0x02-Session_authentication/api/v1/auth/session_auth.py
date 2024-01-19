2451c0cf6cef3b20202766d3f08f7be600819498
Grace_ngigi <gracengigi859@gmail.com>
2024-01-18 18:45:43 -0800

    User ID for Session ID

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -17,7 +17,7 @@ class SessionAuth(Auth):
         rand_id = uuid4()
         self.user_id_by_session_id[str(rand_id)] = user_id
         return str(rand_id)
-    
+
     def user_id_for_session_id(self, session_id: str = None) -> str:
         ''' returns a User ID based on a Session ID '''
         if not isinstance(session_id, str) or session_id is None:
