916b20c88a35902940d04870a130ff20c83d0773
Grace-ngigi <gracengigi859@gmail.com>
2024-01-19 05:57:08 +0300

    Before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -36,7 +36,8 @@ def handle_before_req():
         excluded = [
             '/api/v1/status/',
             '/api/v1/unauthorized/',
-            '/api/v1/forbidden/'
+            '/api/v1/forbidden/',
+            '/api/v1/auth_session/login/'
         ]
         if auth.require_auth(request.path, excluded):
             if auth.authorization_header(request) is None:
