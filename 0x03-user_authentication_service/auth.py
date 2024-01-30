32074fd032fb4420e6cedd6a00f21f994a299658
Grace-ngigi <gracengigi859@gmail.com>
2024-01-30 12:03:01 +0300

    Destroy session

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -57,8 +57,18 @@ class Auth:
 
     def get_user_from_session_id(self, session_id) -> User:
         ''' find user by session id '''
+        if session_id is None:
+            return None
         try:
             user = self._db.find_user_by(session_id=session_id)
         except NoResultFound:
             return None
         return user
+
+    def destroy_session(self, user_id) -> None:
+        ''' Destroy Session '''
+        try:
+            user = self._db.update_user(user_id, session_id=None)
+        except ValueError:
+            return None
+        return None
